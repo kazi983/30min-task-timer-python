1b202b51a4acf64d62008403019b76cfeff85bef
Kazi <kaoru.kajihara.mail@gmail.com>
2026-06-06 13:02:57 -0700

    chore: delete log output line

--- app/models/session_service.py
+++ app/models/session_service.py
@@ -16,16 +16,12 @@ class SessionService:
     def start(self, task_id: str):
         self._task_id = task_id
         self._started_at = datetime.now()
-        print("sessionservice start1", self)
-        print("sessionservice start2", self._task_id)
-        print("sessionservice start3", self._started_at)
 
     def finish(self) -> SessionResult:
         if self._task_id is None:
             return
 
         elapsed = datetime.now() - self._started_at
-        print(elapsed)
 
         return SessionResult(
             task_id=self._task_id, elapsed_minutes=int(elapsed.total_seconds() / 60)
